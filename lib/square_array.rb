f45ed3f8e803646950f942c89f8f426c89f43aab
Olga  Chupp <olga.chupp@gmail.com>
2019-10-24 17:59:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
+  new_arr=[];
       counter = 0
      
     while array[counter] do
-      
+      new_arr<<(array[counter]*array[counter])
       puts array[counter]
       counter += 1
     end
