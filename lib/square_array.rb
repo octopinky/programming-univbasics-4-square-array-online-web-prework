a0b36f8162675390a53525dcecdb9c2de11e9e56
Olga  Chupp <olga.chupp@gmail.com>
2019-10-24 18:00:12 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  new_arr=[];
+      counter = 0
+     
+    while array[counter] do
+      new_arr<<(array[counter]*array[counter])
+      puts array[counter]
+      counter += 1
+    end
+    return new_arr
 end
+
+puts "hello"
