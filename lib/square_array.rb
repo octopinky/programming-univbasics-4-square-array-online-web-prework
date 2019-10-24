5a6f60feecf8f399813eca22a2d904453963245e
Olga  Chupp <olga.chupp@gmail.com>
2019-10-24 17:51:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   counter=0 
   
-  puts square_array.each
+ 
   while array[counter] do
+     puts square_array.each
     #square_array[counter]=array[counter]*array[counter]
     
     counter+=1 
