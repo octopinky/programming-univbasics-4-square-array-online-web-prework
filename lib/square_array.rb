1fd127ba032371c0b1ac3ccc1eebf63e79f83cc3
Olga  Chupp <olga.chupp@gmail.com>
2019-10-24 17:57:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
       counter = 0
-    pets = ["Dog", "Cat", "Fish", "Bird", "Hamster"]
      
-    while pets[counter] do
-      puts pets[counter]
+    while array[counter] do
+      
+      puts array[counter]
       counter += 1
     end
 end
